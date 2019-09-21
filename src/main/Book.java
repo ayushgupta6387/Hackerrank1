8a5629105edaf0b2a377f78e0f18991bbf4bb870
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-09-21 00:37:48 -0700

    use getters

--- src/main/Book.java
+++ src/main/Book.java
@@ -4,5 +4,16 @@ class Book {
     private String authorName;
     private String bookName;
     private String ISBN;
-}
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
 }
