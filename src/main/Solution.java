36b396c02740f8aa3e1028389b92e25231146a71
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-09-21 11:32:58 -0700

    print the output

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -3,17 +3,24 @@ package main;
 import java.util.Scanner;
 
 public class Solution {
-}
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         sc.nextLine().trim();
         Book[] books = new Book[number];
-        for (int i = 0; i <= number; i++) {
-            String bookName = sc.nextLine().trim();
-            String authorName = sc.nextLine().trim();
-            String ISBN = sc.nextLine().trim();
-    }
+        if (number == 0) {
+            System.out.println("N/A");
+        } else {
+            for (int i = 0; i < number; i++) {
+                String bookName = sc.nextLine().trim();
+                String authorName = sc.nextLine().trim();
+                String ISBN = sc.nextLine().trim();
+                books[i] = new Book(bookName, authorName, ISBN);
+            }
+            for (int i = 0; i < books.length; i++) {
+                System.out.println(books[i]);
 
+            }
+        }
     }
+}
