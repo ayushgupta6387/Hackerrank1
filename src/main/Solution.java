9cc93eeb3eb57788494b4bdcb4606c01d1ff0987
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-09-21 11:12:56 -0700

    take input from user 'bookName' 'authorName' 'ISBN'

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -7,6 +7,13 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        int number = sc.nextInt();
+        sc.nextLine().trim();
+        Book[] books = new Book[number];
+        for (int i = 0; i <= number; i++) {
+            String bookName = sc.nextLine().trim();
+            String authorName = sc.nextLine().trim();
+            String ISBN = sc.nextLine().trim();
     }
+
     }
