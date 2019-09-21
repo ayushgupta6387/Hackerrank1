7fb360a6929f4db310cc3bd594de1b85991c6a79
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-09-21 11:09:39 -0700

    use scanner class for input

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,8 +1,12 @@
 package main;
 
+import java.util.Scanner;
+
 public class Solution {
 }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+    }
     }
